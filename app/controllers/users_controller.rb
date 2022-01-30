4d3201bf59ae2c2257067ad4ae0aa833f1613013
NegLoL <6904sasa@gmail.com>
2022-01-30 15:47:35 +0900

    mostly user page comp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   end
 
   def index
+    @users = User.all
   end
 
   def create
@@ -18,7 +19,31 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])#仮
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if user_params_image[:image_name]
+      @user.image_name = "#{@user.id}.jpg"
+      image = user_params_image[:image_name]
+      File.binwrite("public/user_images/#{@user.image_name}", image.read)
+    end
+
+    if @user.update(user_params)
+      flash[:notice] = "ユーザー情報が変更されました"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
   end
 
 
@@ -26,5 +51,10 @@ class UsersController < ApplicationController
     #ストロングパラメータ #image_nameはattributeで初期値入れてる
     def user_params
       params.require(:user).permit(:name, :email, :password)
+    end
+
+    #user画像受け取り用　
+    def user_params_image
+      params.require(:user).permit(:image_name)
     end
 end
